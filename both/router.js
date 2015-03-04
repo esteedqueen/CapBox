0ca4712b5c8a273d36a7faa73fac89f4162193ba
Ben Kissi <benjamin.kissi@meltwater.com>
2015-03-04 02:31:40 +0000

    integrated UI with login and dashboard

--- both/router.js
+++ both/router.js
@@ -1,7 +1,17 @@
-Router.route('/', {
-	name: 'home'
+Router.configure({
+	layoutTemplate:'layout'
 });
+
+Router.map(function(){
+this.route('home',{path:'/'});
+
+this.route('loggedIn', function(){
+		this.render('loggedIn')
+	}, {
+		name:'loggedIn'
+	});
 
 Router.route('/dashboard', {
 	name: 'dashboard'
+});
 });
